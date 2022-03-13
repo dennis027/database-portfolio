1065057f9ac0064722d2aa49c3acee955edaa159
dennis kimani <machariad196@gmail.com>
2022-03-13 23:21:42 +0300

    update

--- app/views.py
+++ app/views.py
@@ -2,18 +2,14 @@ from re import search
 from django.db.models import query
 from django.shortcuts import render
 from rest_framework import generics, permissions
-from rest_framework.response import Response
-from knox.models import AuthToken
+
 from .serializers import *
 from django.contrib.auth import login
 from .models import *
 from rest_framework import permissions
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.views import LoginView as KnoxLoginView
-from rest_framework.permissions import IsAdminUser
+
 from rest_framework import viewsets, generics,permissions
-from rest_framework.response import Response
-from rest_framework.filters import SearchFilter
+
 from rest_framework import filters
 from rest_framework import status
 from rest_framework import generics
